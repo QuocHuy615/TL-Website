e82297b2e67a31965c79f92e7fc3ed5332c3538a
23120272 <lehuy1519@gmail.com>
2026-04-15 16:28:30 +0700

    fix: Render-blocking resources from google font

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.tsx";
 import { AuthProvider } from "./context/CustomerAuthContext.tsx";
 import { CartProvider } from "./context/CartContext.tsx";
 import { Toaster } from "@/components/ui/sonner";
 import { AdminAuthProvider } from "./context/AdminAuthContext.tsx";
+
+requestAnimationFrame(() => {
+  void import("./index.css");
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
